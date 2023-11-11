8c4faba844ec7061c13d59ea4d80b106745801f9
k-galvin <110426866+k-galvin@users.noreply.github.com>
2023-11-10 19:27:08 -0800

    Add firebase and authentication service

---
+++ src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBw7ypyzXk6NecYwQOXWuEcEk7pxaMfpEw',
+  authDomain: 'shelved-b3133.firebaseapp.com',
+  projectId: 'shelved-b3133',
+  storageBucket: 'shelved-b3133.appspot.com',
+  messagingSenderId: '941669756338',
+  appId: '1:941669756338:web:570b066d214a7e27fb48ab'
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+export const auth = getAuth(app)
